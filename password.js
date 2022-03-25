3b33bb36bb0180eae4615777fdc5541d152a8643
the-parthenon <ericinportland@gmail.com>
2022-03-25 11:11:41 -0700

    Added functionality to password authenticator to verify uppercase letters.

--- password.js
+++ password.js
@@ -9,13 +9,30 @@ const userName = "Eric"
 
 console.log(`Welcome to the Password Authenticator ${userName}!`)
 reader.question("Please enter your password to see if it is valid:  ", function(input){
-	passwordLength = input.length;
-    if(passwordLength >= 9){
-        console.log("Look at you, hacker! Acceptable password length.")
+	let passwordLength = input.length;
+    let hasUpperCase = false;
+    
+//This for loop verifies that the string contains an uppercase letter, and also prevents a false positive due to the use of numbers or special characters.
+
+    for (let i = 0; i < input.length; i++) {
+        if (input.charAt(i) === input.charAt(i).toUpperCase() && input.charAt(i) !== input.charAt(i).toLowerCase()) {
+            hasUpperCase = true;
+            // console.log('uppercase:',input.charAt(i));
+            // console.log("Uppercase: ", hasUpperCase);
+        }
     }
-    else{
+    if(passwordLength >= 10 && hasUpperCase === true){
+        console.log("Look at you, hacker! Acceptable password.")
+    }
+    else if(passwordLength < 10){
         console.log("I will not abide disobedience.  Enter a longer password next time.")
     }
-
+    else if(hasUpperCase === false){
+        console.log("Pay attention.  Your password requires an Uppercase letter!")
+    }
+    else{
+        console.log("Something looks weird.")
+    }
+    
 reader.close()
 } )  
